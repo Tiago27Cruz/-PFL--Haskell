f73accec46d541f2f1d7fd538480e41a0f5afa74
Tiago Cruz <tiagocruz057@gmail.com>
2023-12-15 23:44:08 +0000

    template update

--- template.hs
+++ template.hs
@@ -1,10 +1,11 @@
 -- PFL 2023/24 - Haskell practical assignment quickstart
+-- Updated on 15/12/2023
 
 -- Part 1
 
 -- Do not modify our definition of Inst and Code
 data Inst =
-  Push Integer | Add | Mult | Sub | Tru | Fals | Equ | Le | And | Neg | Fetch String | Store String | Noop |
+  Push Integer | Add | Mult | Sub | Tru | Fals | Equ | Le | And | Neg | Fetch String | State String | Noop |
   Branch Code Code | Loop Code Code
   deriving Show
 type Code = [Inst]
@@ -31,14 +32,20 @@ testAssembler code = (stack2Str stack, state2Str state)
 
 -- Examples:
 -- testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
--- testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
--- testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
+-- testAssembler [Fals,Push 3,Tru,State "var",State "a", State "someVar"] == ("","a=3,someVar=False,var=True")
+-- testAssembler [Fals,State "var",Fetch "var"] == ("False","var=False")
 -- testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
 -- testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
 -- testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
 -- testAssembler [Push (-20),Push (-21), Le] == ("True","")
--- testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
--- testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
+-- testAssembler [Push 5,State "x",Push 1,Fetch "x",Sub,State "x"] == ("","x=4")
+-- testAssembler [Push 10,State "i",Push 1,State "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,State "fact",Push 1,Fetch "i",Sub,State "i"]] == ("","fact=3628800,i=1")
+-- If you test:
+-- testAssembler [Push 1,Push 2,And]
+-- You should get an exception with the string: "Run-time error"
+-- If you test:
+-- testAssembler [Tru,Tru,State "y", Fetch "x",Tru]
+-- You should get an exception with the string: "Run-time error"
 
 -- Part 2
 
@@ -58,12 +65,12 @@ parse = undefined -- TODO
 
 -- To help you test your parser
 testParser :: String -> (String, String)
-testParser programCode = (stack2Str stack, state2Str state)
-  where (_,stack,state) = run(compile (parse programCode), createEmptyStack, createEmptyState)
+testParser programCode = (stack2Str stack, store2Str store)
+  where (_,stack,store) = run(compile (parse programCode), createEmptyStack, createEmptyStore)
 
 -- Examples:
 -- testParser "x := 5; x := x - 1;" == ("","x=4")
--- testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1 else y := 2" == ("","y=2")
+-- testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2")
 -- testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;)" == ("","x=1")
 -- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
 -- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
