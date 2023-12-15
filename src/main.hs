c105a21e1098ef1ceba9eb4eb911b0cbdc2a64f4
Tiago Cruz <tiagocruz057@gmail.com>
2023-12-15 23:36:05 +0000

    Add and new cases for run

--- src/main.hs
+++ src/main.hs
@@ -7,7 +7,13 @@ push (Right False) stack = FF:stack
 push (Left n) stack = Value n:stack
 
 -- Receives a stack, adds the two topmost integers and pushes the result to the stack after removing the two topmost integers
--- add :: Stack -> Stack
+add :: Stack -> Stack
+add stack =
+    case stack of
+        (Value n1):(Value n2):rest -> Value (n1 + n2):rest
+        TT:rest -> TT:add rest
+        FF:rest -> FF:add rest
+        _ -> error "Invalid Add Operation"
 
 
 run :: (Code, Stack, State) -> (Code, Stack, State)
@@ -15,6 +21,9 @@ run ([], stack, state) = ([], stack, state)
 run (code:rest, stack, state) = 
     case code of
         Push n -> run (rest, push (Left n) stack, state)
+        Tru -> run (rest, push (Right True) stack, state)
+        Fals -> run (rest, push (Right False) stack, state)
+        Add -> run (rest, add stack, state)
 
 
 testAssembler :: Code -> (String, String)
