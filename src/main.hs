8b9421b69de5a1b38074d319f71a1b4130eafa3b
Tiago Cruz <tiagocruz057@gmail.com>
2023-12-15 19:41:57 +0000

    documentation

--- src/main.hs
+++ src/main.hs
@@ -8,6 +8,7 @@ data Inst =
   deriving Show
 type Code = [Inst]
 
+-- The values of something inside a stack are either an Integer or a boolean.
 data StackValue = Value Integer | TT | FF deriving (Show, Eq)
 type Stack = [StackValue]
 
@@ -22,23 +23,29 @@ type State = HashMap.Map Key Value
 stateExample :: State
 stateExample = HashMap.fromList [("a", Value 3), ("someVar", FF), ("var", TT)]
 
+-- Create an empty stack
 createEmptyStack :: Stack
 createEmptyStack = []
 
+-- Receives a stack and returns a string with the values of the stack separated by commas
 stack2Str :: Stack -> String
 stack2Str = intercalate "," . map showStackValue . reverse
 
+-- Receives a StackValue and returns a string with the value of the StackValue
 showStackValue :: StackValue -> String
 showStackValue (Value x) = show x
 showStackValue TT = "True"
 showStackValue FF = "False"
 
+-- Create an empty state
 createEmptyState :: State
 createEmptyState = HashMap.empty
 
+-- Receives a state and returns a string with the values of the state separated by commas
 state2Str :: State -> String
 state2Str = intercalate "," . map showPair . sortOn fst . HashMap.toList
 
+-- Receives a pair (key, value) and returns a string with the key and the value separated by an equal sign
 showPair :: (Key, Value) -> String
 showPair (k, v) = k ++ "=" ++ showStackValue v
 
