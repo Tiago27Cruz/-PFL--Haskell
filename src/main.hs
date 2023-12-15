6169c22b1935f27e90018368c4b26e661ce636a3
Tiago Cruz <tiagocruz057@gmail.com>
2023-12-15 22:26:12 +0000

    started the run code and added the testAssembler

--- src/main.hs
+++ src/main.hs
@@ -1,9 +1,22 @@
 import DataStructs
 
-pushN :: Either Integer Bool -> Stack -> Stack
-pushN (Right True) stack = TT:stack
-pushN (Right False) stack = FF:stack
-pushN (Left n) stack = Value n:stack
+-- Pushes a StackValue to the stack
+push :: Either Integer Bool -> Stack -> Stack
+push (Right True) stack = TT:stack
+push (Right False) stack = FF:stack
+push (Left n) stack = Value n:stack
+
+-- Receives a stack, adds the two topmost integers and pushes the result to the stack after removing the two topmost integers
+-- add :: Stack -> Stack
+
 
 run :: (Code, Stack, State) -> (Code, Stack, State)
-run = undefined
+run ([], stack, state) = ([], stack, state)
+run (code:rest, stack, state) = 
+    case code of
+        Push n -> run (rest, push (Left n) stack, state)
+
+
+testAssembler :: Code -> (String, String)
+testAssembler code = (stack2Str stack, state2Str state)
+  where (_,stack,state) = run(code, createEmptyStack, createEmptyState)
