0e8305a0d4d149e9330c1205472a5e155a359642
Tiago Cruz <tiagocruz057@gmail.com>
2023-12-15 21:56:18 +0000

    pushN

--- src/main.hs
+++ src/main.hs
@@ -1,4 +1,9 @@
 import DataStructs
 
+pushN :: StackValue -> Stack -> Stack
+pushN TT stack = TT:stack
+pushN FF stack = FF:stack
+pushN n stack = n:stack
+
 run :: (Code, Stack, State) -> (Code, Stack, State)
 run = undefined
