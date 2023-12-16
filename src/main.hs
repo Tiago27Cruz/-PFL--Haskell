90465c2952d2ef9d1f1a990b49407cc9a3704515
Tiago Cruz <tiagocruz057@gmail.com>
2023-12-16 17:15:10 +0000

    Eq

--- src/main.hs
+++ src/main.hs
@@ -27,6 +27,19 @@ sub stack =
         (Value n1):(Value n2):rest -> Value (n1 - n2):rest
         _ -> error "Run-time error"
 
+-- Receives a stack, compares the two topmost values and pushes the result to the stack after removing the two topmost values
+eq :: Stack -> Stack
+eq stack =
+    case stack of
+        (Value n1):(Value n2):rest -> if n1 == n2 then TT:rest else FF:rest
+        TT:TT:rest -> TT:rest
+        FF:FF:rest -> TT:rest
+        TT:FF:rest -> FF:rest
+        FF:TT:rest -> FF:rest
+        _ -> error "Run-time error"
+
+
+
 run :: (Code, Stack, State) -> (Code, Stack, State)
 run ([], stack, state) = ([], stack, state)
 run (code:rest, stack, state) = 
@@ -37,6 +50,7 @@ run (code:rest, stack, state) =
         Add -> run (rest, add stack, state)
         Mult -> run (rest, mult stack, state)
         Sub -> run (rest, sub stack, state)
+        Equ -> run (rest, eq stack, state)
 
 
 testAssembler :: Code -> (String, String)
