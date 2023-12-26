150d83484076bcc74ec760f2b23b951363af8b29
Tiago Cruz <tiagocruz057@gmail.com>
2023-12-26 20:36:32 +0000

    same for run and testAssembler

--- src/assembler.hs
+++ src/assembler.hs
@@ -100,3 +100,35 @@ branch c1 c2 stack =
 loop :: Code -> Code -> Code
 loop c1 c2 =
     c1 ++ [Branch (c2 ++ [Loop c1 c2]) [Noop]]
+
+-- Runs the Code given and can also be given a Stack and a State
+run :: (Code, Stack, State) -> (Code, Stack, State)
+run ([], stack, state) = ([], stack, state)
+run (code:rest, stack, state) = 
+    case code of
+        Push n -> run (rest, push (Left n) stack, state)
+        Tru -> run (rest, push (Right True) stack, state)
+        Fals -> run (rest, push (Right False) stack, state)
+        Add -> run (rest, add stack, state)
+        Mult -> run (rest, mult stack, state)
+        Sub -> run (rest, sub stack, state)
+        Equ -> run (rest, eq stack, state)
+        Le -> run (rest, le stack, state)
+        And -> run (rest, ande stack, state)
+        Neg -> run (rest, neg stack, state)
+        Fetch x -> run(rest, fetch x stack state, state)
+        Store x -> do
+            let (newStack, newState) = store x stack state
+            run(rest, newStack, newState)
+        Noop -> do
+            let (newStack, newState) = noop stack state
+            run(rest, newStack, newState)
+        Branch c1 c2 -> do
+            let (newRest, newStack) = branch c1 c2 stack
+            run(newRest++rest, newStack, state)
+        Loop c1 c2 -> run(loop c1 c2 ++ rest, stack, state)
+
+-- To help you test our assembler
+testAssembler :: Code -> (String, String)
+testAssembler code = (stack2Str stack, state2Str state)
+  where (_,stack,state) = run(code, createEmptyStack, createEmptyState)
--- src/main.hs
+++ src/main.hs
@@ -2,8 +2,6 @@ import Assembler
 import DataStructs
 import Compiler
 
-
-
 buildData :: [String] -> [Stm]
 buildData [] = []
 
@@ -54,33 +52,3 @@ buildBexp [x] =
 parse :: String -> [Stm]
 parse = buildData . lexer
 
-run :: (Code, Stack, State) -> (Code, Stack, State)
-run ([], stack, state) = ([], stack, state)
-run (code:rest, stack, state) = 
-    case code of
-        Push n -> run (rest, push (Left n) stack, state)
-        Tru -> run (rest, push (Right True) stack, state)
-        Fals -> run (rest, push (Right False) stack, state)
-        Add -> run (rest, add stack, state)
-        Mult -> run (rest, mult stack, state)
-        Sub -> run (rest, sub stack, state)
-        Equ -> run (rest, eq stack, state)
-        Le -> run (rest, le stack, state)
-        And -> run (rest, ande stack, state)
-        Neg -> run (rest, neg stack, state)
-        Fetch x -> run(rest, fetch x stack state, state)
-        Store x -> do
-            let (newStack, newState) = store x stack state
-            run(rest, newStack, newState)
-        Noop -> do
-            let (newStack, newState) = noop stack state
-            run(rest, newStack, newState)
-        Branch c1 c2 -> do
-            let (newRest, newStack) = branch c1 c2 stack
-            run(newRest++rest, newStack, state)
-        Loop c1 c2 -> run(loop c1 c2 ++ rest, stack, state)
-
-
-testAssembler :: Code -> (String, String)
-testAssembler code = (stack2Str stack, state2Str state)
-  where (_,stack,state) = run(code, createEmptyStack, createEmptyState)
