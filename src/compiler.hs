037d47673261e44fe124e6684b017989f3e39298
isabel <up202108767@up.pt>
2023-12-28 18:50:06 +0000

    typo

--- src/compiler.hs
+++ src/compiler.hs
@@ -66,7 +66,7 @@ buildData list = do
     case findNotInner [";"] list of -- It will look for the first ";" that's not nested or that belongs to an if statement
         Just index -> do -- If it finds it
             let (stm, rest) = splitAt index list -- It will split the list in two, before and after the first ";" that's not nested
-            if head stm == "(" then buildData (tail (init stm)) -- If it's actually multiple nested statements and not one
+            if head stm == "(" then buildData (tail (init stm)) -- If it's actually multiple nested statements instead of just one
             else case rest of
                 [_] -> [buildStm stm] -- If it's at the last statement
                 _ -> buildStm stm : buildData (tail rest) -- If it's not at the last statement, it will build the statement and call itself again with the rest of the list
